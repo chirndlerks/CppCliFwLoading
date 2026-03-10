d86a4d938cef70a568256aaf595ccb391c3167b6
Ivan Rebo <ivan.rebo@infor.com>
2026-03-10 15:59:20 +0100

    workaround to load clr with System.Runtime.Serialization.EnableUnsafeBinaryFormatterSerialization

--- CppApp/CppApp.cpp
+++ CppApp/CppApp.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include "dotnethosting.h"
 #include "..\CppCliLib\Test.h"
 #include <string>
 
 int main() {
-
+	std::ignore = InitDotnet::InitDotNet();
 	std::cout << MyTest(42);
 	std::string str;
 	std::getline(std::cin, str);
